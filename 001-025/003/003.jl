68099f50c85443fe227acffd354bb90a24ebfeb2
FGFW <yishanju@gmail.com>
2016-04-15 03:35:40 +0800

    003

--- 001-025/003/003.jl
+++ 001-025/003/003.jl
@@ -1,4 +1,13 @@
 #=
+
+https://projecteuler.net/problem=3
 2016年4月7日 04:39:13
+
 =#
-def ff1()
+
+@show @time maximum(keys(factor(600851475143)))
+#=
+  0.747788 seconds (608.27 k allocations: 23.402 MB, 4.43% gc time)
+@time(maximum(keys(factor(600851475143)))) = 6857
+[Finished in 6.6s]
+=#
